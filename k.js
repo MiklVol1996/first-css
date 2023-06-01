fd50afe33af39121f22a2beebe929f86ab0f6ba6
MiklVol <pianomihail10@mail.ru>
2023-06-01 15:53:30 +0300

    first css

---
+++ k.js
@@ -0,0 +1,32 @@
+// let menu = {
+//     width: 100,
+//     height: 160,
+//     title: "My menu",
+//     obj_2: {
+//         width: 100,
+//         height: 160,
+//         title: "My menu",
+//         obj_3: {
+//             width: 100,
+//             height: 160,
+//             title: "My menu"
+//         }
+
+//     }
+// }
+
+// function deep_clone_obj(obj) {
+//     let clone = {};
+//     for (let key in obj) {
+//         if (typeof obj[key] == 'object') {
+//             clone[key] = deep_clone_obj(obj[key]);
+//         }
+//         clone[key] = obj[key];
+//     }
+//     return clone;
+// }
+
+// let clone = deep_clone_obj(menu);
+// console.log(clone);
+
+
